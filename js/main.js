ae2ff21a0871aac1aca47cb50f8202b7acd5a7b5
me4tatelnica <mamiha2010@gmail.com>
2022-11-14 16:16:02 +0300

    Адаптирован Slider

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 $(function () {
-
   $(".slider-blog__inner").slick({
     dots: true,
     prevArrow:
       '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false
+        },
+      },
+
+    ],
   });
 
-  $('.menu__btn').on('click', function () {
-    $('.menu__list').toggleClass('menu__list--active');
-  })
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").toggleClass("menu__list--active");
+  });
 
   var mixer = mixitup(".portfolio__content");
 });
